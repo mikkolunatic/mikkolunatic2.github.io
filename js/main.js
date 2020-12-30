3a43f8909e36ed2fd37368a9d7a3ecb835836d10
Mikael Bedard <bedard.mikael@gmail.com>
2020-12-30 09:16:37 -0500

    New version

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ $(document).ready(function(){
     $("#main").css("margin-left", $(".wrapper-masthead").outerWidth());
   }
 
+  $('[data-filter]').click(function(){
+    var filter = $(this).data('filter');
+
+    $('[data-filter]').removeClass('active');
+    $(this).addClass('active');
+
+    $('#portfolio-grid .card').removeClass('visible');
+
+    if(filter != 0){
+      $('#portfolio-grid').find('.'+filter).addClass('visible');
+    }else{
+      $('#portfolio-grid .card').addClass('visible');
+    }
+  });
+
 });
 
 
